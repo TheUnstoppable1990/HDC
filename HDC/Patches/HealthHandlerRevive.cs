930d22532a9fb0eb039e3abf2948a31769406e53
TheUnstoppable1990 <theunstoppable1990@hotmail.com>
2022-03-01 06:37:16 -0500

    Create HealthHandlerRevive.cs

---
+++ HDC/Patches/HealthHandlerRevive.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using HDC.Extentions;
+
+namespace HDC.Patches
+{
+    [Serializable]
+    [HarmonyPatch(typeof(HealthHandler),"Revive")]
+    class HealthHandlerRevive
+    {
+        private static void Prefix(HealthHandler __instance, bool isFullRevive = true)
+        {
+            if (isFullRevive)
+            {
+                ((CharacterData)Traverse.Create(__instance).Field("data").GetValue()).stats.GetAdditionalData().holyLightCharge = 0f;
+            }
+        }
+    }
+}
